d837b168f0b44c165b420305a607dbad8cdc3a84
armandfardeau <fardeauarmand@gmail.com>
2023-01-24 19:16:22 +0100

    Setting for clicked state

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,45 @@
 import {useState} from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import {Configuration, OpenAIApi} from "openai"
 
 function App() {
     const [prompt, setPrompt] = useState('')
     const [result, setResult] = useState('')
+    const [clicked, setClicked] = useState(false)
     const configuration = new Configuration({
         apiKey: import.meta.env.VITE_OPENAI_API_KEY
     });
     const openai = new OpenAIApi(configuration);
     const generate_image = async () => {
+        setClicked(true)
         const response = await openai.createImage({
             prompt: prompt, n: 1, size: "1024x1024"
         });
 
-        setResult(response.data.data[0].url)
+        setClicked(false)
+        setResult(response.data.data[0])
     }
-    const hasResult = result.length > 0
-    const hasPrompt = prompt.length > 0
+    const hasResult = result.url?.length > 0
 
     return (<div className="app-main">
-        {hasResult ? <h1>Result for '{prompt}'</h1>: <h1>Generate an image using OpenAI API</h1>}
-        {hasResult ? <img src={result} alt="Generated image" className="app-image"/> : <></>}
-        {!hasResult && hasPrompt ? <h2>Generating image...</h2> : <></>}
-        {!hasResult && hasPrompt ? <div className="app-loader"></div> : <></>}
+        {hasResult ? <h1>Result for '{prompt}'</h1> : <h1>Generate an image using OpenAI API</h1>}
+        {hasResult ? <img src={result.url} alt="Generated image" className="app-image"/> : <></>}
+        {!hasResult && clicked ? <h2>Generating image...</h2> : <></>}
+        {!hasResult && clicked ? <div className="app-loader"></div> : <></>}
         <input
             type="text"
             className="app-input"
+            disabled={!hasResult && clicked }
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
             placeholder="Type something to generate your image..."
         />
         <div className="app-actions">
-            <button onClick={generate_image}>Generate Image</button>
+            <button disabled={!hasResult && clicked } onClick={generate_image}>Generate Image</button>
             {hasResult ? <button onClick={() => {
                 setResult('');
                 setPrompt('')
+                setClicked(false)
             }}>Clear</button> : <></>}
         </div>
     </div>)
